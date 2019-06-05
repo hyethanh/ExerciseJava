5ba45e2f3acb873986970b8b91e8e127fbf421ac
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-05 16:36:07 +0700

    Init

---
+++ src/main/java/Exercise5.java
@@ -0,0 +1,16 @@
+public class Exercise5 {
+    public String Fibonacci(int n) {
+        int c;
+        int a = 1, b = 1;
+        String result = "1 1 ";
+
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            result += c;
+            a = b;
+            b = c;
+        }
+        result = result.substring(0);
+        return result;
+    }
+}
