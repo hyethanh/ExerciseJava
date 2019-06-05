0d10ebd0f583ec45fd1c13c51ad2d212a6531df5
Võ Quang Hòa <hvquang@openwt.com>
2019-06-05 09:00:41 +0700

    Add exercise 2

---
+++ src/test/java/Exercise2Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Exercise2Test {
+
+    @Test
+     public void test_mul(){
+        //assertEquals(new Exercise2().mul(100),2*2*5*5;)
+    }
+}
