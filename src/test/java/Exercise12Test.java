a260a48a1b14618c299e4332fa5520463be4db1f
Võ Quang Hòa <hvquang@openwt.com>
2019-06-05 09:06:27 +0700

    Add exercise 12

---
+++ src/test/java/Exercise12Test.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Exercise12Test {
+
+    @Test
+    public void test_expression(){
+        assertEquals(new Exercise12().expression(3),2.6667, 0.00001);
+    }
+}
