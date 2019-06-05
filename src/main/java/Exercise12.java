ab2a13141b16660b24089392c312eaf3e643197c
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-05 15:35:00 +0700

    Init

--- src/main/java/Exercise12.java
+++ src/main/java/Exercise12.java
@@ -1,37 +1,12 @@
 public class Exercise12 {
 
-    public int factorial(int m) {
-        int p = 1;
-        if (m == 0 || m == 1)
-            return p;
-        else {
-            for (int i = 2; i <= m; i++) {
-                p = p * i;
+    public double expression (int n) {
+        double s = 1;
+        double temp = 1;
+        for(int i=1;i<=n;i++){
+                temp /= i;
+                s += temp;
             }
-        }
-        return p;
+        return s;
     }
-
-        public double expression (int n) {
-            double s = 1.0;
-
-            for (int i = 1; i <= n; i++) {
-                System.out.println(factorial(i));
-                s = s + (1 / factorial(i));
-
-            }
-            return s;
-        }
-
-        public static void main(String [] args){
-        Exercise12 a = new Exercise12();
-        double x=1.0;
-            System.out.println(a.factorial(3));
-            for (int i = 1; i <= 3; i++) {
-                x = x + (float)(1 / a.factorial(i));
-            }
-        System.out.println(x);
-
-        }
-
 }
--- src/test/java/Exercise12Test.java
+++ src/test/java/Exercise12Test.java
@@ -6,6 +6,8 @@ public class Exercise12Test {
 
     @Test
     public void test_expression(){
-        assertEquals(new Exercise12().expression(3),2.6667, 0.00001);
+        assertEquals(new Exercise12().expression(0),1.000,0.001);
+        assertEquals(new Exercise12().expression(3),2.667,0.001);
     }
+
 }
