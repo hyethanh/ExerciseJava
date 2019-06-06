0e63aabae406df87c55b99a66bcf5c9a6a7df3e0
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-06 15:51:58 +0700

    Init

--- src/main/java/Exercise5.java
+++ src/main/java/Exercise5.java
@@ -1,16 +1,17 @@
 public class Exercise5 {
     public String Fibonacci(int n) {
-        int c;
+        int c=0;
         int a = 1, b = 1;
-        String result = "1 1 ";
+        String result = "1 1";
 
-        for (int i = 3; i <= n; i++) {
+        while(c<n)
+        {
             c = a + b;
-            result += c+" ";
+            result += " "+c;
             a = b;
             b = c;
         }
-        result = result.substring(0);
+        result = result.substring(0,result.length()-3);
         return result;
     }
 }
--- src/test/java/Exercise5Test.java
+++ src/test/java/Exercise5Test.java
@@ -6,7 +6,7 @@ public class Exercise5Test {
 
     @Test
     public void test_Fibonacci(){
-        assertEquals(new Exercise5().Fibonacci(9),"1 1 2 3 5 8");
         assertEquals(new Exercise5().Fibonacci(22),"1 1 2 3 5 8 13 21");
+        assertEquals(new Exercise5().Fibonacci(9),"1 1 2 3 5 8");
     }
 }
