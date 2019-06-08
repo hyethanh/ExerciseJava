866e1ec1d531f5a4afa6229422c18ec23b3f8dee
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-08 09:04:34 +0700

    init

--- src/main/java/Exercise1.java
+++ src/main/java/Exercise1.java
@@ -2,7 +2,7 @@ public class Exercise1 {
 
     public int ucln(int a, int b) {
         if (a == 0 || b == 0)
-            return (a+b);
+            return (a + b);
         else {
             while (a != b) {
                 if (a > b)
@@ -14,9 +14,9 @@ public class Exercise1 {
         return a;
     }
 
-    public int bcnn(int a, int b){
+    public int bcnn(int a, int b) {
         int x;
-        x= ucln(a,b);
-        return a * b / ucln(a, b) ;
+        x = ucln(a, b);
+        return a * b / x;
     }
 }
--- src/test/java/Exercise1Test.java
+++ src/test/java/Exercise1Test.java
@@ -6,11 +6,18 @@ public class Exercise1Test {
 
     @Test
     public void test_ucln() {
-            assertEquals(new Exercise1().ucln(10, 8), 2);
+        assertEquals(new Exercise1().ucln(10, 8), 2);
 
     }
+
     @Test
     public void test_bcnn() {
-        assertEquals(new Exercise1().bcnn(10,8),40);
+        assertEquals(new Exercise1().bcnn(10, 8), 40);
+    }
+
+    @Test
+    public void test_ucbc() {
+        assertEquals(new Exercise1().ucln(0, 3), 3);
+        assertEquals(new Exercise1().bcnn(0, 3), 0);
     }
 }
