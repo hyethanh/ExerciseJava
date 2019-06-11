ff01305e396067cff510530be464fb4d3998fe90
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-11 10:18:52 +0700

    init

---
+++ src/main/java/Exercise16.java
@@ -0,0 +1,48 @@
+
+public class Exercise16 {
+    public double edgeTriangle(int x1, int y1, int x2, int y2) {
+        double z;
+        z = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+        return z;
+    }
+
+    public String Triangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+        double a, b, c,a1,b1,c1;
+        String result = "";
+        double p;
+        double s;
+        double p1;
+        a = edgeTriangle(x1, y1, x2, y2);
+        b = edgeTriangle(x1, y1, x3, y3);
+        c = edgeTriangle(x2, y2, x3, y3);
+
+        a1 = Math.sqrt(a);
+        b1 = Math.sqrt(b);
+        c1 = Math.sqrt(c);
+
+        if (a1 + b1 <= c1 || b1 + c1 <= a1 || a1 + c1 <= b1)
+            return "Khong phai la ba canh cua tam giac";
+        else {
+            p = a1 + b1 + c1;
+            p1 = p / 2;
+            s = Math.sqrt(p1 * (p1 - a1) * (p1 - b1) * (p1 - c1));
+
+            p = (double) Math.round(p * 100) / 100;
+            s = (double) Math.round(s * 100) / 100;
+
+            if ((a  + b  == c)  || (a  + c  == b)  || ( b + c  == a ))
+                result = "La 3 canh cua tam giac vuong co chu vi " + p + ", dien tich " + s;
+            else {
+                if (a1 == b1 && a1 == c1 && b1 == c1)
+                    result = "La 3 canh tam giac deu co chu vi " + p + ", dien tich " + s;
+                else {
+                    if (a1 == b1 || b1 == c1 || a1 == c1)
+                        result = "La 3 canh tam giac can co chu vi " + p + ", dien tich " + s;
+                    else
+                        result = "La 3 canh tam giac thuong co chu vi " + p + ", dien tich " + s;
+                }
+            }
+        }
+        return result;
+    }
+}
