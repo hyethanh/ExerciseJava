c52bebed6ce80808cc7d1f4a18f070285b383d09
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-12 10:19:21 +0700

    init

--- src/main/java/Exercise19.java
+++ src/main/java/Exercise19.java
@@ -1,15 +1,19 @@
 public class Exercise19 {
-    public int C(int k, int n) {
+
+    public int c(int k, int n) {
         if ((k == 0) || (k == n)) return 1;
         if (k == 1) return n;
-        return C(k - 1, n - 1) + C(k - 0, n - 1);
+
+        return c(k - 1, n - 1) + c(k , n - 1);
     }
 
     public int[] pascalTriangle(int n) {
         int[] a = new int[n + 1];
-        for (int i = 0; i <= n + 1; i++) {
-            a[i] = C(i - 0, n + 1);
+
+        for (int i = 0; i < n + 1; i++) {
+            a[i] = c(i, n );
         }
+
         return a;
     }
 }
--- src/test/java/Exercise19Test.java
+++ src/test/java/Exercise19Test.java
@@ -1,18 +1,20 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise19Test {
     @Test
     public void test_C() {
-        assertEquals(new Exercise19().C(1, 2), 2);
-        assertEquals(new Exercise19().C(2, 5), 10);
-        assertEquals(new Exercise19().C(0, 3), 1);
+        assertEquals(new Exercise19().c(1, 2), 2);
+        assertEquals(new Exercise19().c(2, 5), 10);
+        assertEquals(new Exercise19().c(0, 3), 1);
     }
 
+    @Test
     public void test_pascalTriangle() {
-        assertEquals(new Exercise19().pascalTriangle(2), new int[]{1, 2, 1});
-        assertEquals(new Exercise19().pascalTriangle(4), new int[]{1, 4, 6, 4, 1});
-        assertEquals(new Exercise19().pascalTriangle(5), new int[]{1, 5, 10, 10, 5, 1});
+        assertArrayEquals(new Exercise19().pascalTriangle(2), new int[]{1, 2, 1});
+        assertArrayEquals(new Exercise19().pascalTriangle(4), new int[]{1, 4, 6, 4, 1});
+        assertArrayEquals(new Exercise19().pascalTriangle(5), new int[]{1, 5, 10, 10, 5, 1});
     }
 }
