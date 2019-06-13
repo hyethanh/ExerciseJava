99212441032b0cb0a772f4e7a583cc0679065892
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-13 11:10:49 +0700

    edited

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -2,14 +2,15 @@ public class Exercise3 {
     public String mullPrime(int n){
         String result="";
         int i = 2;
-        while(n>0) {
+        while(n>=i) {
             if(n%i==0) {
                 result += i+"*";
                 n=n/i;
             }
             else i++;
+
         }
-        result = result.substring(0,result.length()-4);
+        result = result.substring(0,result.length()-1);
         return result;
     }
 }
