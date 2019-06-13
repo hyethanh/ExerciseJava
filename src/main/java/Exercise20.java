d8740e4c0f581c38fa2eaabd26582de5904faa5f
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-13 10:58:29 +0700

    edited

--- src/main/java/Exercise20.java
+++ src/main/java/Exercise20.java
@@ -1,9 +1,9 @@
-public class Exercise20{
-    public int sum_Fib(int n){
+public class Exercise20 {
+    public int sumFib(int n) {
         int c;
-        int a=1,b=1;
-        int sum =2;
-        for(int i=3;i<=n;i++) {
+        int a = 1, b = 1;
+        int sum = 2;
+        for (int i = 3; i <= n; i++) {
             c = a + b;
             sum += c;
             a = b;
--- src/test/java/Exercise20Test.java
+++ src/test/java/Exercise20Test.java
@@ -3,11 +3,12 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise20Test {
-    @Test
-    public void test_sumFib(){
 
-        assertEquals(new Exercise20().sum_Fib(2),2);
-        assertEquals(new Exercise20().sum_Fib(6),20);
-        assertEquals(new Exercise20().sum_Fib(5),12);
+    @Test
+    public void test_sumFib() {
+
+        assertEquals(new Exercise20().sumFib(2), 2);
+        assertEquals(new Exercise20().sumFib(6), 20);
+        assertEquals(new Exercise20().sumFib(5), 12);
     }
 }
