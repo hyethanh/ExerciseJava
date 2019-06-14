25633e055b7b0320f3cee54e7b5ef0e1939049dc
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-14 10:44:25 +0700

    edited

--- src/test/java/Exercise24Test.java
+++ src/test/java/Exercise24Test.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class Exercise24Test {
     @Test
     public void test_validNumber() {
-        assertEquals(new Exercise24().countValidNumber(3), 4);
-        assertEquals(new Exercise24().countValidNumber(14), 7);
+        assertEquals(new Exercise24().countFibNumber(3), 4);
+        assertEquals(new Exercise24().countFibNumber(14), 7);
     }
 }
