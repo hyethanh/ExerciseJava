10304bcfdcff04f59ff2399596cd8877817d834f
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-14 09:44:26 +0700

    init

--- src/main/java/ArrayExercise4.java
+++ src/main/java/ArrayExercise4.java
@@ -1,27 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class ArrayExercise4 {
 
-    public Integer[] arangeArray(Integer[] arr) {
+    public int[] arangeArray(int[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
-                if (arr[i] % 2 == 1 && arr[j] % 2 == 0) {
-                    int temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
+                if (arr[i] % 2 == 0 && arr[j] % 2 == 1) {
+                    continue;
                 }
-            }
+                if (arr[i] % 2 == arr[j] % 2) {
+                    if ((arr[i] > arr[j]) == (arr[i] % 2 == 0)) {
+                        continue;
+                    }
+                }
 
-            if (arr[i] % 2 == 0) {
-                Arrays.sort(arr, 0, i + 1, Collections.reverseOrder());
-            } else {
-                Arrays.sort(arr, i, arr.length);
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
             }
-
         }
 
         return arr;
