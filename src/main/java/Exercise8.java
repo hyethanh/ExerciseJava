9cc8f03433d4ddd0c2938771e3138c5c29b05d92
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-17 10:00:35 +0700

    edited

--- src/main/java/Exercise8.java
+++ src/main/java/Exercise8.java
@@ -1,13 +1,13 @@
 public class Exercise8 {
-    public int factorial(int n){
+
+    public int factorial(int n) {
+
         int p = 1;
-        if(n==0||n==1)
-            return p;
-        else {
-            for (int i = 2; i <= n; i++) {
-                p = p * i;
-            }
+
+        for (int i = 2; i <= n; i++) {
+            p = p * i;
         }
+
         return p;
     }
 }
--- src/test/java/Exercise8Test.java
+++ src/test/java/Exercise8Test.java
@@ -5,8 +5,11 @@ import static org.junit.Assert.assertEquals;
 public class Exercise8Test {
 
     @Test
-    public void test_factorial(){
+    public void test_factorial() {
 
-        assertEquals(new Exercise8().factorial(4),24);
+        assertEquals(new Exercise8().factorial(4), 24);
+        assertEquals(new Exercise8().factorial(1), 1);
+        assertEquals(new Exercise8().factorial(0), 1);
+
     }
 }
