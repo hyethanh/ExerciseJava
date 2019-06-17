e65d9bf111cc725400bf41868cdfd8a7bfae2461
Võ Quang Hòa <hvquang@openwt.com>
2019-06-17 11:20:41 +0700

    Format

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -1,20 +1,19 @@
 public class Exercise3 {
-    public String mullPrime(int n){
+    public String mullPrime(int n) {
 
-        StringBuilder result= new StringBuilder();
+        StringBuilder result = new StringBuilder();
         int i = 2;
 
-        while(n>=i) {
-            if(n%i==0) {
+        while (n >= i) {
+            if (n % i == 0) {
 
                 result.append(i);
-                n=n/i;
+                n = n / i;
 
-                if(n>1){
+                if (n > 1) {
                     result.append("*");
                 }
-            }
-            else{
+            } else {
                 i++;
             }
         }
