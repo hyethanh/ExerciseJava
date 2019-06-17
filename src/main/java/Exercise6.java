fdd12c73cacff27420c57253280543e6aceae4b0
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-17 10:38:28 +0700

    init

--- src/main/java/Exercise6.java
+++ src/main/java/Exercise6.java
@@ -1,26 +1,35 @@
 public class Exercise6 {
-    public void equation_2nd(int a,int b, int c){
-        float x1,x2;
-        if(a==0)
-        {
-            if(b==0) System.out.println("Phuong trinh vo nghiem");
+    public String equation2nd(int a,int b, int c) {
+
+        float x1, x2;
+        float delta = b * b - 4 * a * c;
+
+        if (a == 0) {
+            if (b == 0) {
+                return "Phuong trinh vo nghiem";
+            }
             else {
-                x1 = -c/b;
-                System.out.println("Nghiem phuong trinh x = " +x1);
+                x1 = -c / b;
+                return "Nghiem phuong trinh x = " + x1;
             }
         }
-        float delta = b*b -4*a*c;
-        if (delta > 0)
-        {
-            x1 = (float)((-b + Math.sqrt(delta) / (2*a)));
-            x2 = (float)((-b - Math.sqrt(delta) / (2*a)));
-            System.out.println("Phuong trinh co 2 nghiem phan biet");
-            System.out.println("x1 = "+x1);
-            System.out.println("x2 = "+x2);
+
+        if (delta > 0) {
+            x1 = (float) ((-b + Math.sqrt(delta) / (2 * a)));
+            x2 = (float) ((-b - Math.sqrt(delta) / (2 * a)));
+            return "Phuong trinh co 2 nghiem phan biet x1 = " + x1+" x2 = " + x2;
+
         }
-        else if(delta == 0 )
-            System.out.println("Phuong trinh co nghiem kep: x1 = x2 =  "+(-b/(2*a)));
-        else
-            System.out.println("Phuong trinh vo nghiem");
+
+        else if (delta == 0) {
+
+            return "Phuong trinh co nghiem kep: x1 = x2 = " + (-b / (2 * a));
+
+        }
+
+        else {
+            return "Phuong trinh vo nghiem";
+        }
+
     }
 }
