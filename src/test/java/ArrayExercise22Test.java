767b5f8dc7900a2b802ed8f6490dafaff955ee5a
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-18 14:19:16 +0700

    init

---
+++ src/test/java/ArrayExercise22Test.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ArrayExercise22Test {
+
+    @Test
+
+    public void test_cloneArray(){
+
+        assertArrayEquals(new ArrayExercise22().cloneArray(
+                new int[][]{
+                        {1,2,3,4,5},
+                        {6,7,8,9,1},
+                        {3,4,5,6,7}},4,2),
+                new int[][]{
+                        {1,2,3,4},
+                        {6,7,8,9}
+                });
+
+        assertArrayEquals(new ArrayExercise22().cloneArray(
+                new int[][]{
+                        {1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}},2,3),
+                new int[][]{
+                        {1,2,4},
+                        {5,6,8},
+                        {9,10,12},
+                });
+    }
+}
