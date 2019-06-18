e1e35f1c359bab6d889dc6a3a44c6486a166412a
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-06-18 14:21:06 +0700

    init

--- src/main/java/ArrayExercise22.java
+++ src/main/java/ArrayExercise22.java
@@ -1,25 +1,25 @@
 public class ArrayExercise22 {
 
-    public int[][] cloneArray(int[][] arr,int x,int y){
+    public int[][] cloneArray(int[][] arr, int x, int y) {
 
-        int[][] result= new int[arr.length-1][arr[0].length-1];
+        int[][] result = new int[arr.length - 1][arr[0].length - 1];
 
-        int m=0;
+        int m = 0;
 
-        for(int i=0;i<arr.length;i++){
-            int n=0;
-            if(i==y){
+        for (int i = 0; i < arr.length; i++) {
+            int n = 0;
+            if (i == y) {
                 continue;
             }
 
-            for(int j=0;j<arr[0].length;j++){
-                if(j==x){
+            for (int j = 0; j < arr[0].length; j++) {
+                if (j == x) {
                     continue;
                 }
-                result[m][n]=arr[i][j];
+                result[m][n] = arr[i][j];
                 n++;
             }
-           m++;
+            m++;
         }
 
         return result;
--- src/test/java/ArrayExercise22Test.java
+++ src/test/java/ArrayExercise22Test.java
@@ -6,28 +6,28 @@ public class ArrayExercise22Test {
 
     @Test
 
-    public void test_cloneArray(){
+    public void test_cloneArray() {
 
         assertArrayEquals(new ArrayExercise22().cloneArray(
                 new int[][]{
-                        {1,2,3,4,5},
-                        {6,7,8,9,1},
-                        {3,4,5,6,7}},4,2),
+                        {1, 2, 3, 4, 5},
+                        {6, 7, 8, 9, 1},
+                        {3, 4, 5, 6, 7}}, 4, 2),
                 new int[][]{
-                        {1,2,3,4},
-                        {6,7,8,9}
+                        {1, 2, 3, 4},
+                        {6, 7, 8, 9}
                 });
 
         assertArrayEquals(new ArrayExercise22().cloneArray(
                 new int[][]{
-                        {1,2,3,4},
-                        {5,6,7,8},
-                        {9,10,11,12},
-                        {13,14,15,16}},2,3),
+                        {1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}}, 2, 3),
                 new int[][]{
-                        {1,2,4},
-                        {5,6,8},
-                        {9,10,12},
+                        {1, 2, 4},
+                        {5, 6, 8},
+                        {9, 10, 12},
                 });
     }
 }
