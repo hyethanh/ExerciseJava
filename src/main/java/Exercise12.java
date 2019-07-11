01e17a19d1c87ca18581e8e704e9a5cb79cf5800
Võ Quang Hòa <voquanghoa@gmail.com>
2019-07-11 10:53:00 +0700

    Undelete files

--- src/main/java/Exercise12.java
+++ src/main/java/Exercise12.java
@@ -4,9 +4,9 @@ public class Exercise12 {
         double s = 1;
         double temp = 1;
         for(int i=1;i<=n;i++){
-            temp /= i;
-            s += temp;
-        }
+                temp /= i;
+                s += temp;
+            }
         return s;
     }
 }
