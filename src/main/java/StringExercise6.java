04d035c8b1d9d9c28d52d674d3d800d47a576411
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-07-18 15:33:26 +0700

    init

--- src/main/java/StringExercise6.java
+++ src/main/java/StringExercise6.java
@@ -1,24 +1,20 @@
-import java.util.ArrayList;
-
 public class StringExercise6 {
 
-    public String upDownCaseString(String input){
+    public String upDownCaseString(String input) {
 
         char[] str = input.toCharArray();
         StringBuilder result = new StringBuilder();
 
-        for(char s:str){
-            if(Character.isLowerCase(s)){
-                s=(char) (s - 32);
-            }
-            else if(Character.isUpperCase(s)){
-                s=(char) (s + 32);
+        for (char s : str) {
+            if (Character.isLowerCase(s)) {
+                s = (char) (s - 32);
+            } else if (Character.isUpperCase(s)) {
+                s = (char) (s + 32);
             }
             result.append(s);
         }
 
-
         return result.toString();
-    }
 
+    }
 }
--- src/test/java/StringExercise6Test.java
+++ src/test/java/StringExercise6Test.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertEquals;
 public class StringExercise6Test {
 
     @Test
-    public void upDownCaseString_test(){
+    public void upDownCaseString_test() {
 
-        assertEquals(new StringExercise6().upDownCaseString("abCDEfgh"),"ABcdeFGH");
+        assertEquals(new StringExercise6().upDownCaseString("abCDEfgh"), "ABcdeFGH");
 
     }
 }
