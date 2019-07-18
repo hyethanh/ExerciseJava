b8640b16384868ea25d7a3d11d206c49026541b0
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-07-18 16:50:02 +0700

    init

--- src/test/java/StringExercise8Test.java
+++ src/test/java/StringExercise8Test.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class StringExercise8Test {
+
+    @Test
+    public void standardize_test(){
+
+        assertEquals(new StringExercise8().standardize("ha noi la thu do, Viet.Nam"),"Ha noi la thu do, viet. Nam");
+}
 }
