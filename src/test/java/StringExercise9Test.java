1539d6c6459944b5b9969875e6acba42054a8426
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-07-20 10:54:08 +0700

    init

--- src/test/java/StringExercise9Test.java
+++ src/test/java/StringExercise9Test.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.assertEquals;
 public class StringExercise9Test {
 
     @Test
-    public void abridgeString_test(){
+    public void abridgeString_test() {
 
-        assertEquals(new StringExercise9().abridgeString("abcccceeeeeefd"),"abc4e6fd");
+        assertEquals(new StringExercise9().abridgeString("abcccceeeeeefd"), "abc4e6fd");
+        assertEquals(new StringExercise9().abridgeString("abbbbbbbbbbbbbc"), "ab13c");
+
     }
 }
