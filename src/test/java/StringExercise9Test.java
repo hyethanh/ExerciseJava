a0c6a45e3306d6bec8d30f45dfbbb86bdbdfb031
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-07-23 11:58:47 +0700

    init

--- src/test/java/StringExercise9Test.java
+++ src/test/java/StringExercise9Test.java
@@ -9,11 +9,9 @@ public class StringExercise9Test {
 
         assertEquals(new StringExercise9().abridgeString("abcccceeeeeefd"), "abc4e6fd");
         assertEquals(new StringExercise9().abridgeString("abbbbbbbbbbbbbc"), "ab13c");
-<<<<<<< HEAD
+
         assertEquals(new StringExercise9().abridgeString("cc"), "c2");
-=======
         assertEquals(new StringExercise9().abridgeString("abbbbbbbbbbbbbcc"), "ab13c2");
->>>>>>> 297dad69774370779687103cf1b6e3c0cd326df7
 
     }
 }
