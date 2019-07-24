c0e33d2ace2c7cea0b68c3cdfb624d4f3d024e53
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-07-24 09:09:51 +0700

    init

--- src/test/java/Exercise25Test.java
+++ src/test/java/Exercise25Test.java
@@ -3,10 +3,13 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise25Test {
+
     @Test
-    public void test_countNumber(){
-        assertEquals(new Exercise25().countNumber(0,4),4);
-        assertEquals(new Exercise25().countNumber(0,1),2);
-        assertEquals(new Exercise25().countNumber(0,21),8);
+    public void test_countNumber() {
+
+        assertEquals(new Exercise25().countNumber(0, 4), 4);
+        assertEquals(new Exercise25().countNumber(0, 1), 2);
+        assertEquals(new Exercise25().countNumber(0, 21), 8);
+
     }
 }
