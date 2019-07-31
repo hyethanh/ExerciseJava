70e2053757741cb3cd2189265795071b1b4e8880
thanhhuyen1208 <pthhuyen123@gmail.com>
2019-07-31 16:10:17 +0700

    update

--- src/test/java/Exercise30Test.java
+++ src/test/java/Exercise30Test.java
@@ -6,13 +6,16 @@ public class Exercise30Test {
 
     @Test
     public void test_convert() {
+
         assertEquals(new Exercise30().convert(150), "CL");
         assertEquals(new Exercise30().convert(2288), "MMCCLXXXVIII");
         assertEquals(new Exercise30().convert(8), "VIII");
         assertEquals(new Exercise30().convert(940), "CMXL");
-        assertEquals(new Exercise30().convert(99),"XCIX");
-        assertEquals(new Exercise30().convert(4),"IV");
-        assertEquals(new Exercise30().convert(510),"DX");
-        assertEquals(new Exercise30().convert(421),"CDXXI");
+
+        assertEquals(new Exercise30().convert(99), "XCIX");
+        assertEquals(new Exercise30().convert(4), "IV");
+        assertEquals(new Exercise30().convert(510), "DX");
+        assertEquals(new Exercise30().convert(421), "CDXXI");
+
     }
 }
